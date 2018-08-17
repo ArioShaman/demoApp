f999bd50a7ea7a742770ab764c108865e4d64ebe
ArioShaman <ariosha6@gmail.com>
2018-08-17 22:36:28 +0300

    resing covers of album

--- src/components/image/album-list.ts
+++ src/components/image/album-list.ts
@@ -2,16 +2,19 @@ export const ALBUM = [
   {
     id: 1,
     name: 'Pet',
-    cover: 'assets/imgs/album-01.jpg'
+    cover: 'assets/imgs/album-01.jpg',
+    smCover:  'assets/imgs/album-01-sm.jpg',
   },
   {
     id: 2,
     name: 'Food',
-    cover: 'assets/imgs/album-02.jpg'
+    cover: 'assets/imgs/album-02.jpg',
+    smCover:  'assets/imgs/album-02-sm.jpg',
   },
   {
     id: 3,
     name: 'Car',
-    cover: 'assets/imgs/album-03.jpg'
+    cover: 'assets/imgs/album-03.jpg',
+    smCover:  'assets/imgs/album-03-sm.jpg',
   }
 ]
