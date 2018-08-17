9e0a462ea26b6b539be09bab276d31a16b61106f
ArioShaman <ariosha6@gmail.com>
2018-08-17 22:29:46 +0300

    album covers in template

--- src/components/image/album-list.ts
+++ src/components/image/album-list.ts
@@ -1,11 +1,17 @@
 export const ALBUM = [
   {
-    id: 1
+    id: 1,
+    name: 'Pet',
+    cover: 'assets/imgs/album-01.jpg'
   },
   {
-    id: 2
+    id: 2,
+    name: 'Food',
+    cover: 'assets/imgs/album-02.jpg'
   },
   {
-    id: 3
+    id: 3,
+    name: 'Car',
+    cover: 'assets/imgs/album-03.jpg'
   }
 ]
